e6f08f695e580a2a6f1e5c3b884fb1b4f00d9073
joy masemola <jbmasemza@yahoo.com>
2019-11-18 11:10:03 +0200

    secont commit

--- addtion.py
+++ addtion.py
@@ -4,7 +4,10 @@ def add(*args):
     for num in args:
         total = total + num
     return total
-    
+
+print(add(0,0))
+print(add(-1,-1))
+print(add(4,5))    
 print(add(1,2,3,4))
 
 def multiply(num1, *args):
@@ -13,4 +16,5 @@ def multiply(num1, *args):
         total = total * num
     return total
     
+print(multiply(1,2))
 print(multiply(1,2,3,4))
--- test_function.py
+++ test_function.py
@@ -3,7 +3,11 @@ from addtion import multiply
 
 
 def test_add():
+ assert add(0,0) == 0
+ assert add(-1,-1) == -2
+ assert add(4,5) == 9
  assert add(1,2,3,4) == 10
 
-def test_multioly():
+def test_multiply():
+ assert multiply(1,2) == 2
  assert multiply(1,2,3,4) == 24
