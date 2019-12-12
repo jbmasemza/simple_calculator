114a23ff91ac7155534648652eac2fa68c0a5d53
jbmasemza <jbmasemza@yahoo.com>
2019-12-12 12:20:14 +0200

    Add files via upload

---
+++ test_simple_calculator.py
@@ -0,0 +1,13 @@
+from addtion import add
+from addtion import multiply
+
+
+def test_add():
+ assert add(0,0) == 0
+ assert add(-1,-1) == -2
+ assert add(4,5) == 9
+ assert add(1,2,3,4) == 10
+
+def test_multiply():
+ assert multiply(1,2) == 2
+ assert multiply(1,2,3,4) == 24
